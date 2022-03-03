db423cb9511d5a043469300639f05ea676398031
CassianoCardoso <dj_kassiano@hotmail.com>
2022-03-03 20:43:48 -0300

    aula 03

--- main.js
+++ main.js
@@ -1,31 +1,60 @@
+/*  abre e fecha o menu quando clicar no icone: hamburguer e x */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
 
-/*Abre  e fecha o menu hmburguer */ 
-const nav = document.querySelector('#header nav');
-const toggle = document.querySelectorAll('nav .toggle');
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
 
-for (const element of toggle){
-  element.addEventListener('click',function () {
-    nav.classList.toggle('show');
-  });
-};
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
 
-/*Quando clicar no item do menu vai esconder o menu */
-const links = document.querySelectorAll('nav ul li a');
-for (const link of links){
-  link.addEventListener('click',function(){
-    nav.classList.remove('show');
-  });
-};
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
 
-/*Mudar o header da página quando rolar o scroll do mouse */
-const header = document.querySelector('#header');
-const navHeight = header.offsetHeight;
+/* mudar o header da página quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
 
-window.addEventListener('scroll',function(){
-  if(window.scrollY >= navHeight){
-    header.classList.add('scroll');
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
   }
-  else{
-    header.classList.remove('scroll');
-  }
-});
+})
+
+/* Testimonials carousel slider swiper */
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard: true
+})
+
+/* ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .image, #home .text,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials
+  #contact .text, #contact .links
+  `,
+  { interval: 100 }
+)
